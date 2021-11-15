bf42836e616b8e693b3b0492dc78bc8423f41b65
Erik Zander <a.deepergray@gmail.com>
2021-11-15 21:30:10 +0100

    20min

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-const App = () => {
-  return <div>Hello world!</div>;
-};
+import { Button, makeStyles} from '@material-ui/core'
+
+import { Person } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) =>({
+  button:{
+    ... theme.myButton
+  },
+}));
+
+
+
+function App() {
+  const classes = useStyles();
+  return <div>
+  
+
+  </div>;
+
+
+
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { theme } from "./theme";
+import { ThemeProvider } from '@material-ui/styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
