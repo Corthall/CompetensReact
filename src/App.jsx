c53ae63d4a4a6c80410331f8d9e9a37ac43deca3
Stefan <stefan.f.sand@afry.com>
2021-11-17 18:52:56 +0100

    Added Grid components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { makeStyles } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
 import Navbar from "./components/Navbar";
 import Feed from "./components/Feed";
+import Leftbar from "./components/Leftbar";
+import Rightbar from "./components/Rightbar";
 
 const useStyles = makeStyles((theme) => ({}));
 
@@ -9,6 +11,17 @@ function App() {
   return (
     <div class="pageContent" >
       <Navbar />
+      <Grid container>
+        <Grid item sm={2} xs={2}>
+          <Leftbar />
+        </Grid>
+        <Grid item sm={7} xs={10}>
+          <Feed />
+        </Grid>
+        <Grid item sm={3}>
+          <Rightbar />
+        </Grid>
+      </Grid>
       <Feed />
     </div>
   );
